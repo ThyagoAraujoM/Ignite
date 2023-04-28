9ab7186bdcc8225a2e1bedbf62f4fcc1431c37f5
Thyago Motta <thyagomotta@rubeus.com.br>
2023-04-27 23:37:42 -0300

    inicio da edição 2023

---
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import "./global.css";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
