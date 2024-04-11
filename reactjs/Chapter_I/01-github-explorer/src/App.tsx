45182b505dcc1273ddd7bdfbd5d7dabb19b60d62
ThyagoAraujo <thyagoaraujopessoal@gmail.com>
2024-04-11 11:43:38 -0300

    criando branch react-native

--- reactjs/Chapter_I/01-github-explorer/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import RepositoryList from "./components/RepositoryList";
-import "./styles/global.scss";
-
-export default function App() {
-  return <RepositoryList />;
-}
