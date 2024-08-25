8480dfc97f91ef077a34ec9779965d9c334fc8f2
ThyagoAraujo <thyagoaraujopessoal@gmail.com>
2024-08-25 15:36:48 -0300

    feat: aprendendo a criar estilos e estilizar ActivityIndicator / loading

--- 2-Interfaces-Navegação-Armazenamento-local/igniteteams/App.tsx
+++ 2-Interfaces-Navegação-Armazenamento-local/igniteteams/App.tsx
@@ -3,7 +3,6 @@ import { defaultTheme } from "@theme/default";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
 import { Loading } from "@components/Loading";
 
 export default function App() {
@@ -12,14 +11,8 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        {fontsLoaded ? (
-          <>
-            <StatusBar translucent style="light"></StatusBar>
-            <Groups />
-          </>
-        ) : (
-          <Loading />
-        )}
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+        {fontsLoaded ? <Groups /> : <Loading />}
       </ThemeProvider>
     </>
   );
--- 2-Interfaces-Navegação-Armazenamento-local/igniteteams/src/components/Loading/index.tsx
+++ 2-Interfaces-Navegação-Armazenamento-local/igniteteams/src/components/Loading/index.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-
-import { ActivityIndicator, View } from "react-native";
-
 import { Container, LoadingIndicator } from "./styles";
 
 export function Loading() {
