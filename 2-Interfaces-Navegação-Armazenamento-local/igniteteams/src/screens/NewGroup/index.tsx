5579addef76402d740a620fd73dc89c7a0dbfaf1
ThyagoAraujo <thyagoaraujopessoal@gmail.com>
2024-08-25 22:12:33 -0300

    feat: aprendendo a utililizar o helper css do style-components e refatorando os estilos

--- 2-Interfaces-Navegação-Armazenamento-local/igniteteams/src/screens/NewGroup/index.tsx
+++ 2-Interfaces-Navegação-Armazenamento-local/igniteteams/src/screens/NewGroup/index.tsx
@@ -13,7 +13,7 @@ export function NewGroup() {
       <Content>
         <Icon />
         <Highlight title="Nova turma" subTitle="crie a turma para adicionar as pessoas" />
-        <Input />
+        <Input placeholder="Nome da turma" />
         <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
