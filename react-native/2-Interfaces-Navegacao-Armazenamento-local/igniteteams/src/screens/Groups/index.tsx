98ea7ce608ae78157455fc160beb51084a8b20b6
ThyagoAraujo <thyagoaraujopessoal@gmail.com>
2024-08-29 21:41:19 -0300

    feat: aprendendo a utilizar a navegação de tela e duas formas de se configurar, uma sendo na mão e a outra utilizando useNavigation

--- react-native/2-Interfaces-Navegacao-Armazenamento-local/igniteteams/src/screens/Groups/index.tsx
+++ react-native/2-Interfaces-Navegacao-Armazenamento-local/igniteteams/src/screens/Groups/index.tsx
@@ -7,11 +7,28 @@ import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-export function Groups() {
+type RootParamList = {
+  groups: undefined;
+  new: undefined;
+  players: {
+    group: string;
+  };
+};
+
+type Props = {
+  navigation: NativeStackNavigationProp<RootParamList, "groups">;
+};
+
+export function Groups({ navigation }: Props) {
   const [groups, setGroups] = useState([]);
-  const navigation = useNavigation();
 
+  // const navigation = useNavigation();
+
+  // Possível utilizar navigation sendo passado pelo props do componente e configurado suas propriedades e tipagem,
+  // Ou utilizar o próprio hook do react native useNavigation que já volta de forma mais enxuta as mesmas funçãoes
+  // e funcionalidades
   function handleNewGroup() {
     navigation.navigate("new");
   }
