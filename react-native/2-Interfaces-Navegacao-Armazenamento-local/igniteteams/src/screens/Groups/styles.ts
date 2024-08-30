4e17604d3d09afca2c72d352d3e8c6862d1d8fc6
ThyagoAraujo <thyagoaraujopessoal@gmail.com>
2024-08-29 23:01:10 -0300

    feast: utilizando o safe area context

--- react-native/2-Interfaces-Navegacao-Armazenamento-local/igniteteams/src/screens/Groups/styles.ts
+++ react-native/2-Interfaces-Navegacao-Armazenamento-local/igniteteams/src/screens/Groups/styles.ts
@@ -1,4 +1,5 @@
 // import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
 // export const styles = StyleSheet.create({
@@ -10,7 +11,7 @@ import styled from "styled-components/native";
 //   },
 // });
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
