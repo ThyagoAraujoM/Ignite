977b4ffc0542efb1c1a35f0b04e8f7d3dda414f6
Pluto <thyagoaraujopessoal@gmail.com>
2025-07-10 18:59:24 -0300

    limpando branch

--- react/Edicao-2025/requests/src/helpers/api.ts
+++
@@ -1,4 +0,0 @@
-export const fetcher = (route: string)=> fetch(`http://localhost:3000/${route}`).then((res)=> res.json());
-
-
-export const api = (route: string, options: RequestInit)=> fetch(`http://localhost:3000/${route}`, options).then((res)=> res.json());
