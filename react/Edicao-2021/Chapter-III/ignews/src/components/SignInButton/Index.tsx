ffe8693468983b761fe61a0f4a3d1765f2c520d0
Pluto <thyagoaraujopessoal@gmail.com>
2025-08-24 01:21:52 -0300

    feat: primeiros passos com o next auth

--- react/Edicao-2021/Chapter-III/ignews/src/components/SignInButton/Index.tsx
+++ react/Edicao-2021/Chapter-III/ignews/src/components/SignInButton/Index.tsx
@@ -2,17 +2,19 @@ import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
-export function SignInButton() {
-  const isUserLoggedIn = true;
+import {signIn, signOut, useSession}  from "next-auth/react";
 
-  return isUserLoggedIn ? (
+export function SignInButton() {
+  const {data: sessions, status} = useSession();
+  console.log(sessions);
+  return sessions ? (
     <button className={styles.SignInButton} type='button'>
       <FaGithub color='#04d361' />
-      Pluto
-      <FiX color='#737380' className={styles.closeIcon} />
+      {sessions.user.name}
+      <FiX color='#737380' className={styles.closeIcon} onClick={()=>signOut()} />
     </button>
   ) : (
-    <button className={styles.SignInButton} type='button'>
+    <button className={styles.SignInButton} type='button' onClick={()=>{signIn("github")}}>
       <FaGithub color='#eba417' />
       Sign in with Github
     </button>
