e2d3c59ffd19aefcd5f4645519d1ea7a7249bc09
Pluto <thyagoaraujopessoal@gmail.com>
2025-08-26 00:06:00 -0300

    feat: redirecionamento para o Stripe

--- react/Edicao-2021/Chapter-III/ignews/src/components/SubscribeButton/index.tsx
+++ react/Edicao-2021/Chapter-III/ignews/src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 type SubscribeButtonProps = {
   priceId: string;
 };
@@ -7,18 +9,29 @@ type SubscribeButtonProps = {
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const {data: session} = useSession();
 
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn("github");
       return;
     }
 
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({sessionId});
+
+
+    } catch (error) {
+      alert(error.message);
+    }
 
     // criação do da checkout session
   }
 
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button type='button' onClick={handleSubscribe} className={styles.subscribeButton}>
       Subscribe now
     </button>
   );
